a03b97fbd1b687edde4db7ffe74fac8b8a9d194d
mru24 <mru24@yahoo.com>
2018-11-18 11:52:17 +0000

    Edited

--- js/main.js
+++ js/main.js
@@ -1,18 +1,28 @@
-$(window).scroll(function()
-    {
-        if($(this).scrollTop()>250) {
-          $('.scrollToTop').fadeIn();
-        } else {
-          $('.scrollToTop').fadeOut();
-        }
-      }
-    );
 
-jQuery(document).ready(function($) {
-
-  $.scrollTo('#top');
-
-  $('a').click(function(event) {
-    $.scrollTo($(this).attr('href'), 500);
-  });
+// SHOW SCROLL TO TOP ARROW
+var arrow = document.querySelector('#scrollToTop');
+window.addEventListener('scroll', function () {
+  // console.log(window.pageYOffset);
+  if(window.pageYOffset > 150) {
+    arrow.classList.add('showArrow');
+    arrow.style.transform = "scale(1)";
+  } else {
+    arrow.classList.remove('showArrow');
+    arrow.style.transform = "scale(0)";
+  }
 });
+
+// SCROLL TO ELEMENT
+var el = document.querySelectorAll('a:not(.footerA)');
+for (var i = 0; i < el.length; i++) {
+  el[i].addEventListener('click', function (e) {
+    e.preventDefault();
+    var element = e.srcElement.getAttribute('href');
+    var target = document.querySelector(element);
+
+    target.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
